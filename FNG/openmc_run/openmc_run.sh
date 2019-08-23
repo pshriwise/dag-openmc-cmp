e818ebccd014273f35410ebd29cd3191cb761ce2
zxkjack123 <zxkjack123@163.com>
2019-08-23 14:55:18 -0500

    update openmc_run.sh

--- FNG/openmc_run/openmc_run.sh
+++ FNG/openmc_run/openmc_run.sh
@@ -2,7 +2,7 @@
 # this file is called submit-script.sh
 #SBATCH --partition=univ2		# default "univ", if not specified
 #SBATCH --time=1-00:00:00		# run time in days-hh:mm:ss
-#SBATCH --nodes=1			# require 2 nodes
+#SBATCH --nodes=4			# require 2 nodes
 #SBATCH --ntasks-per-node=16            # (by default, "ntasks"="cpus")
 #SBATCH --mem-per-cpu=4000		# RAM per CPU core, in MB (default 4 GB/core)
 #SBATCH --error=job.%J.err
@@ -14,5 +14,5 @@
 # Example for non-SLURM-compiled code:
 #module load mpi/gcc/openmpi-1.6.4
 #mpirun -n 32 /home/username/mpiprogram
-cd $HOME/opt/dag-openmc-cmp/openmc_run
-mpiexec -n 16 openmc 
+cd $HOME/opt/dag-openmc-cmp/FNG/openmc_run
+mpiexec -n 64 openmc 
